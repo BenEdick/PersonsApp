7b33e926e0becfca3ee8fbf828c0b83df8bcdef1
BenEdick <brge85@gmail.com>
2013-09-08 12:00:51 -0700

    User App CRUD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+
+  before_action :find_user, only: %w(edit update show destroy)
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash.notice = "Congratulations! A new account has been created"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update user_params
+      flash.notice = "User has been updated"
+      redirect_to user_url(@user)
+    else
+      render "edit"
+    end
+  end
+
+ def destroy
+  @user.destroy
+
+  redirect_to users_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :email)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserApp::Application.config.secret_key_base = '1315268e33ca26eb6cab602016675076380d96f9a633e071e799996044771fee351dfc133e6e178a8b51df375ec163e9d91459e2d76d7cc7c4931af289656399'
